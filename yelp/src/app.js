85a7e8a89b18774c261523acc977002b53b13b4f
Hugo Dutra <hugow.dutra@gmail.com>
2016-07-10 19:37:47 -0300

    config webpack

--- yelp/src/app.js
+++ yelp/src/app.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import styles from './styles/module.css';
+import 'font-awesome/css/font-awesome.css'
+import './app.css'
+import styles from './styles.module.css'
 
 const App = React.createClass({
-	render: function() {
+	render: ()=> {
 		return (
-			<div className={styles["container"]}>
-				OLARRRR
+			<div className={styles.wrapper}>
+				<h1><i className="fa fa-star"></i>Enviroment: {__NODE_ENV__}</h1>	
 			</div>
 		);
 	}
--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -7,11 +7,63 @@ const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
+const getConfig = require('hjs-webpack');
+const dotenv = require('dotenv');
 
 const NODE_ENV = process.env.NODE_ENV;
 const isDev    = NODE_ENV === 'development';
 
-const getConfig = require('hjs-webpack');
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+	path: join(root, 'config', `${NODE_ENV}.config.js`),
+	silent: true,
+});
+
+const envVariables = Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+	Object.keys(envVariables).reduce((memo, key) => {
+    	const val = JSON.stringify(envVariables[key]);
+    	memo[`__${key.toUpperCase()}__`] = val;
+    	return memo;
+  	}, {
+    	__NODE_ENV__: JSON.stringify(NODE_ENV)
+  	});
+
+console.log(defines);
+
+var config = getConfig({
+ 	isDev: isDev,
+ 	in: join(src, 'app.js'),
+ 	out: dest,
+ 	clearBeforeBuild: true
+})
+
+
+config.plugins = [
+	new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
+config.resolve.root = [src, modules]
+config.resolve.alias = {
+	'css': join(src, 'styles'),
+	'containers': join(src, 'containers'),
+	'components': join(src, 'components'),
+	'utils': join(src, 'utils')
+}
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+ 	const found = loaders.filter(l => l &&
+    	l.loader && l.loader.match(match));
+	return found ? found[0] : null;
+}
+
+const cssloader = findLoader(config.module.loaders, matchCssLoaders);
 
 var config = getConfig({
 	isDev: isDev,
@@ -20,10 +72,30 @@ var config = getConfig({
 	clearBeforeBuild: true
 });
 
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
+cssloader.loader = newloader.loader;
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
 config.postcss = [].concat([
 	require('precss')({}),
 	require('autoprefixer')({}),
 	require('cssnano')({})
 ]);
+
+config.plugins = [ new webpack.DefinePlugin(defines) ].concat(config.plugins);
 
 module.exports = config;
